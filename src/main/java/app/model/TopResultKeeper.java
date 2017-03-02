a2c8023756815f3946639fddbbcb3b39f9eb1f06
Tuxarb <tuxarb777@yandex.ru>
2017-03-02 23:34:13 +0300

    fixed isTopResult(...) at TopResultKeeper.

--- src/main/java/app/model/TopResultKeeper.java
+++ src/main/java/app/model/TopResultKeeper.java
@@ -10,18 +10,32 @@ public class TopResultKeeper {
         setValuesByDefault();
     }
 
-    boolean isTopResult(long capacity, long speed, long runtime) {
-        if (this.runtime != Long.MAX_VALUE && this.runtime > 1.85 * runtime) {
+    boolean isTopResult(long newCapacity, long newSpeed, long newRuntime) {
+        if (this.runtime == Long.MAX_VALUE) {
+            update(newCapacity, newSpeed, newRuntime);
+            return true;
+        }
+        if (newRuntime < 0.5 * this.runtime) {              //new value runtime is too small
             return false;
         }
-        if (this.runtime >= runtime &&
-                (this.capacity > capacity || this.capacity < 0.9 * capacity)) {
-            this.runtime = runtime;
-            this.capacity = capacity;
-            this.speed = speed;
+        if (newCapacity >= this.capacity * 0.75 && (       //new value capacity is not smaller than prev not more than a quarter
+                (newRuntime < this.runtime &&               //new value runtime is less than prev
+                        (newCapacity <= this.capacity ||     //new value capacity is not more than prev
+                                //new value capacity is not more than prev not more than a quarter and and the runtime difference is not less than 20 ms
+                                (newCapacity <= this.capacity * 1.25 && this.runtime - newRuntime >= 20))) ||
+                        (newRuntime == this.runtime && newCapacity < this.capacity) ||
+                        (newRuntime > this.runtime && newRuntime - this.runtime <= 15 &&
+                                newCapacity <= 0.95 * this.capacity))) {
+            update(newCapacity, newSpeed, newRuntime);
             return true;
         }
         return false;
+    }
+
+    private void update(long newCapacity, long newSpeed, long newRuntime) {
+        this.runtime = newRuntime;
+        this.capacity = newCapacity;
+        this.speed = newSpeed;
     }
 
     public void reset() {
